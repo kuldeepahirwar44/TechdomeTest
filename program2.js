62f18649636ff1e22ef5a8615ff45bab18b137fe
Kuldeep Ahirwar <120416755+kuldeepahirwar44@users.noreply.github.com>
2024-02-23 13:36:48 +0530

    Update program2.js

--- program2.js
+++ program2.js
@@ -1,10 +1,27 @@
-/**
- * @param {string} s
- * @return {number}
- */
-var romanToInt = function(s) {
-    
-};
+function romanToInt(s) {
+    const romanValues = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
+
+    let result = 0;
+    for (let i = 0; i < s.length; i++) {
+        if (i < s.length - 1 && romanValues[s[i]] < romanValues[s[i + 1]]) {
+            result -= romanValues[s[i]];
+        } else {
+            result += romanValues[s[i]];
+        }
+    }
+
+    return result;
+}
 
 
-module.exports={romanToInt}
+console.log(romanToInt("III"));     // Output: 3
+console.log(romanToInt("LVIII"));   // Output: 58
+console.log(romanToInt("MCMXCIV")); // Output: 1994
