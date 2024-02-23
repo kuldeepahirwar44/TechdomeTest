8601b834076cccc4a6ebc27207624304288fd754
Kuldeep Ahirwar <120416755+kuldeepahirwar44@users.noreply.github.com>
2024-02-23 13:37:26 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -19,3 +19,8 @@ function isValid(s) {
 
     return stack.length === 0;
 }
+
+console.log(isValid("()"));      // Output: true
+console.log(isValid("()[]{}"));  // Output: true
+console.log(isValid("(]"));      // Output: false
+
